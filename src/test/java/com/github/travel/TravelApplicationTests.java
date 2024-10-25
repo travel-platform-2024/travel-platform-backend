7b611fe57d470a097df698d005b38ccaafd0930a
oeun42 <oeunzzang@naver.com>
2024-10-25 14:59:03 +0900

    initialize travel-backend

---
+++ src/test/java/com/github/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
